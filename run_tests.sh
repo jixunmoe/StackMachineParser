e61655b234e1b7c4b4d0f5bb391428ea2d4fb38c
Jixun Wu <jixun.moe@gmail.com>
2019-04-26 10:59:11 +0100

    add scripts to help analyse performance

---
+++ run_tests.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+export RUN_FIGURES=YES
+
+# NOTE: Do not commit changes to the next line
+export TEST_NAME=CONTROLLED_RAM_PORT
+
+
+export LOG_FILE=logs/TEST_LOG_${TEST_NAME}.log
+export LOG_FILTER_FILE=logs/TEST_RESULT_${TEST_NAME}.log
+mkdir -p logs
+
+
+./gradlew test -i --rerun-tasks > $LOG_FILE
+cat $LOG_FILE | \
+  grep -v "STANDARD_ERROR" | \
+  grep -i "Program completed\|SmSimulatorTest" | \
+  tee $LOG_FILTER_FILE
